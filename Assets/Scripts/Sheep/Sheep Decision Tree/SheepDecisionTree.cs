d7b2b3f8e40893353f2de6283acfed6c015b45cf
solangebordeta <solangebordeta@live.com>
2025-06-19 15:47:55 -0300

    SheepDecisionTree
    - agregue una carpeta dentro de Sheep con el script, pero solamnete tiene variables escritas

---
+++ Assets/Scripts/Sheep/Sheep Decision Tree/SheepDecisionTree.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepDecisionTree : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform enemy;
+    [SerializeField] bool isHiding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
